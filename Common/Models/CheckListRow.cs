b70ab728915a0a981537d741ca15e67ba42bd9ba
ofirelarat <96ofir11@gmail.com>
2021-04-22 21:46:13 +0300

    add checklist row bind model

---
+++ Common/Models/CheckListRow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Models
+{
+    public class CheckListRow
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Person_1_value { get; set; }
+        public string Person_2_value { get; set; }
+        public string Comments { get; set; }
+    }
+}
